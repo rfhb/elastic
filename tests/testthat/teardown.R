6a6340cea24e378941faf39a96bea3bf8758b10f
Ralf Herold <ralf.herold@gmx.net>
2026-01-23 21:57:22 +0100

    github action - increase opensearch number, teardown iteratively

--- tests/testthat/teardown.R
+++ tests/testthat/teardown.R
@@ -1,4 +1,5 @@
 unlink("mtcars.json")
 unlink("mtcarslist.json")
 x <- elastic::connect(port = Sys.getenv("TEST_ES_PORT"))
-invisible(elastic::index_delete(x, index="*", verbose = FALSE))
+ins <- names(elastic::alias_get(x))
+for (i in ins) invisible(elastic::index_delete(x, index = i, verbose = FALSE))
